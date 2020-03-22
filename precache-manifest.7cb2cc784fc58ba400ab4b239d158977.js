be8d004c0c7a28ddf035309142fc023cc4ba12eb
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2020-03-22 18:57:33 +0100

    Verification

---
+++ precache-manifest.7cb2cc784fc58ba400ab4b239d158977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139616bacdeefc4c6d05f7d07dc91396",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58a0f62c44438845513a",
+    "url": "/static/css/main.3ee95d29.chunk.css"
+  },
+  {
+    "revision": "504a9327d36c88c83fab",
+    "url": "/static/js/2.0c5c9347.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.0c5c9347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a0f62c44438845513a",
+    "url": "/static/js/main.69f0ea1e.chunk.js"
+  },
+  {
+    "revision": "39dc4db9d0648d66cdbf",
+    "url": "/static/js/runtime-main.1c083ac1.js"
+  }
+]);
