3f323cd864608bcc19a3f58b28c9faaa44904132
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2020-08-20 00:01:53 +0200

    Video youtube alike

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 
-const KEY =  'AIzaSyDKVIeX6WnUXWCbV6umSYqjIdHAqgokbEA';
+
+// const KEY =  'AIzaSyDKVIeX6WnUXWCbV6umSYqjIdHAqgokbEA';
 // const KEY =  'AIzaSyBYWhLo85aWCtXCKCMORWD67SGOguf550g';
+const KEY =  'AIzaSyDB63CSNJRcYHZQj7DlygHP6FfHDNLqm5Q';
 
 
 export default axios.create({ 
